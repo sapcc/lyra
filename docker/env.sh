9e61f984bf3256d8b1b04dfb5333a2aa14a1dff7
Fabian Ruff <fabian.ruff@sap.com>
2016-02-05 14:55:00 +0100

    automatically run migrations before starting up

--- docker/env.sh
+++ docker/env.sh
@@ -5,4 +5,12 @@ if [ -f "$ENV_FILE" ]; then
   echo Sourcing environment file "$ENV_FILE" ...
   . "$ENV_FILE"
 fi
+
+if rake db:version 2>/dev/null; then
+  echo rake db:migrate
+  rake db:migrate
+else
+  echo rake db:setup db:migrate
+  rake db:setup db:migrate
+fi
 exec "$@"
