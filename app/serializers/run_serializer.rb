d4d3d9bc5e8ed4f0401c0846b268d4e423eb1bf7
Fabian Ruff <fabian.ruff@sap.com>
2016-03-31 10:24:08 +0200

    add automation_id and automation_name to run

--- app/serializers/run_serializer.rb
+++ app/serializers/run_serializer.rb
@@ -1,7 +1,15 @@
 class RunSerializer < ActiveModel::Serializer
-  attributes :id, :log, :created_at, :updated_at, :repository_revision, :state, :log, :jobs, :owner
+  attributes :id, :log, :created_at, :updated_at, :repository_revision, :state, :log, :jobs, :owner, :automation_id, :automation_name
 
   def id
     object.id.to_s
+  end
+
+  def automation_name
+    object.automation.name
+  end
+
+  def automation_id
+    object.automation_id.to_s
   end
 end
