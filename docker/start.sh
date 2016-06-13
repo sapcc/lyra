e506be389b133d2efaac40d97ef868b7b60d2265
Fabian Ruff <fabian.ruff@sap.com>
2016-06-13 23:14:31 +0200

    Avoid runing db:setup if database connection fails

--- docker/start.sh
+++ docker/start.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 set -e 
 
-if ! rake db:abort_if_pending_migrations >/dev/null 2>&1; then
-  if rake db:version 2>/dev/null; then
-    echo Migrating database 
-    rake db:migrate
-  else
-    echo Initializing database 
+export PGHOST=${POSTGRES_SERVICE_HOST:-postgres}
+export PGUSER=${MONSOON_DB_USER:-postgres}
+export PGPASSWORD=$MONSOON_DB_PASSWORD
+export PGDATABASE=${MONSOON_DB_NAME:-monsoon-automation_production}
+
+dbs=$(psql -tlA)
+if echo "$dbs" | grep -q $PGDATABASE; then
+  if psql -c "\dt schema_migrations" | grep -q "No matching relations found"; then
+    echo "Table schema_migrations not found. Running db:setup"
     rake db:setup
-  fi 
+  else
+    echo "Running db:migrate"
+    rake db:migrate
+  fi
+else
+  echo "Database $PGDATABASE not found. Running db:setup"
+  rake db:setup
 fi
+unset PGHOST PGUSER PGPASSWORD PGDATABASE
 
 exec puma -C config/puma.rb 
